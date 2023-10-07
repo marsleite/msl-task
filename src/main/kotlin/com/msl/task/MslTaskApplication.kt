9d4c5d11ae940064ce8707e0d2a41e4980fba6bc
Marcelo <marcelo.leite@mercadolivre.com>
2023-10-07 11:33:02 -0300

    [feat] first commit

---
+++ src/main/kotlin/com/msl/task/MslTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.msl.task
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MslTaskApplication
+
+fun main(args: Array<String>) {
+  runApplication<MslTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/msl/task/MslTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.msl.task
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MslTaskApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
